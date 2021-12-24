b01ce342d707283c9943ef5646d6bfae4ae5183a
Iqbal Zauqul <iqbalzauqul@gmail.com>
2021-12-24 16:22:23 +0700

    Initial Commit

---
+++ stockbit-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockbit-test
+//
+//  Created by Iqbal Zauqul Adib on 24/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
