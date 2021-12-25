f39ba1a381dbe7d60c04fb00ab803ff28d0ad62b
Iqbal Zauqul <iqbalzauqul@gmail.com>
2021-12-25 13:17:59 +0700

    made toplistservice into singleton

--- stockbit-test/Services/TopListService.swift
+++ stockbit-test/Services/TopListService.swift
@@ -13,6 +13,10 @@ protocol TopListServiceProtocol {
 
 struct TopListService: TopListServiceProtocol {
     
+    private init() {}
+    
+    static let shared = TopListService()
+    
     func fetchTopList(completion: @escaping (Result<TopListResponse,APIErrorResult>)->() ) {
         APIManager.shared.execute(endpoint: Endpoint.getTopList(limit:"50",currency: "USD"),completion: completion)
     }
